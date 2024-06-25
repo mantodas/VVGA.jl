788aca5ceb425b89c2b36e806340def1273cd459
Documenter.jl <documenter@juliadocs.github.io>
2024-06-25 16:39:05 +0000

    build based on ce1552d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#VVGA.jl","page":"VVGA.jl","title":"VVGA.jl","text":"","category":"section"},{"location":"","page":"VVGA.jl","title":"VVGA.jl","text":"Documentation for VVGA.jl","category":"page"}]
+}
